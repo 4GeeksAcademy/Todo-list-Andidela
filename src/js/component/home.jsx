da4cbe74d2d45afc65f36714e338abf6bd0d739d
Andidela <andidela83@gmail.com>
2024-11-02 02:26:58 +0000

    Proyecto Finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,62 @@
-import React from "react";
+import React, { useState } from 'react';
+import '/workspaces/Todo-list-Andidela/src/styles/index.css';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function App() {
+	const [tasks, setTasks] = useState([]);
+	const [input, setInput] = useState('');
 
-//create your first component
-const Home = () => {
+	const handleAddTask = (e) => {
+		if (e.key === 'Enter' || e.type === 'click') {
+			if (input.trim() !== '') {
+				setTasks([...tasks, input]);
+				setInput('');
+			}
+		}
+	};
+
+	const handleDeleteTask = (index) => {
+		setTasks(tasks.filter((task, i) => i !== index));
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+		<div className="paper" style={{width: 500, marginLeft: 400, marginTop: 100}}>
+			<div className="todo-app">
+				<h1>To-Do List</h1>
+				<div className="input-container">
+					<input
+						type="text"
+						placeholder="Add a new task"
+						value={input}
+						onChange={(e) => setInput(e.target.value)}
+						onKeyDown={handleAddTask}
+					/>
+					<button onClick={handleAddTask}>Add</button>
+				</div>
 
-export default Home;
+				{/* Task List */}
+				<ul className="task-list">
+					{tasks.length === 0 ? (
+						<p className="no-tasks">No tasks, add a task</p>
+					) : (
+						tasks.map((task, index) => (
+							<li
+								key={index}
+								className="task-item"
+								onMouseEnter={() => setHover(index)}
+								onMouseLeave={() => setHover(null)}
+							>
+								<span>{task}</span>
+								<button className="delete-btn" onClick={() => handleDeleteTask(index)}>
+									x
+								</button>
+							</li>
+						))
+					)}
+				</ul>
+			</div>
+		</div>
+
+	);
+}
+
+export default App;
